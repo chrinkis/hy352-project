04c1c94e279196e8d5001472d9f7a5f80ed98c3d
Alex <csd4764@csd.uoc.gr>
2022-12-06 15:52:10 +0200

    build(compile.sh): add option for build and run

--- compile.sh
+++ compile.sh
@@ -4,3 +4,11 @@ mkdir -p build &&
     cd build &&
     cmake .. &&
     make hy352-project
+
+if [[ $? -eq 0 && $1 = "-r" ]]; then
+    echo
+    echo "Executing:"
+    echo "---"
+    echo
+    ./hy352-project
+fi
