ec75ac2ada8be5aaf2137195188fd63d96793e34
Alex <csd4764@csd.uoc.gr>
2022-12-11 18:27:00 +0200

    feat(value): add pure virtual methods

--- src/jsonlang/values/value.h
+++ src/jsonlang/values/value.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <string>
+
 namespace jsonlang {
 namespace values {
 
-class Value {};
+class Value {
+ public:
+  virtual int get_size() const = 0;
+  virtual bool is_emtpy() const = 0;
+  virtual bool has_key(const std::string& key) const = 0;
+  virtual const char* get_type() const = 0;
+
+  virtual operator std::string() const = 0;
+};
 
 }  // namespace values
 }  // namespace jsonlang
