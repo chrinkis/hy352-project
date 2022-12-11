41148f479aec8241aac883d3daa717ba78c5b748
Alex <csd4764@csd.uoc.gr>
2022-12-11 22:49:53 +0200

    feat(sequence): implement `Sequence`

--- src/collections/sequence.h
+++ src/collections/sequence.h
@@ -1,8 +1,25 @@
 #pragma once
 
+#include <vector>
+
 namespace collections {
 
 template <typename T>
-class Sequence {};
+class Sequence {
+  std::vector<T> data;
+
+ public:
+  Sequence<T>& operator,(const T& element) {
+    this->data.push_back(element);
+
+    return *(this);
+  }
+
+ public:
+  using Iterator = typename std::vector<T>::iterator;
+
+  Iterator begin() { return this->data.begin(); }
+  Iterator end() { return this->data.end(); }
+};
 
 }  // namespace collections
