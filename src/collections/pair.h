6bccd0e8cf8a5aed8fbf24fe68ebadcc0470d2ba
Alex <csd4764@csd.uoc.gr>
2022-12-13 00:20:04 +0200

    feat(pair): don't require heap allocation

--- src/collections/pair.h
+++ src/collections/pair.h
@@ -1,20 +1,17 @@
 #pragma once
 
-#include <memory>
-
 namespace collections {
 
 template <typename F, typename S>
 class Pair {
-  std::shared_ptr<F> first;
-  std::shared_ptr<S> second;
+  F first;
+  S second;
 
  public:
-  Pair(const F& _first, const S& _second)
-      : first(_first.clone_to_heap()), second(_second.clone_to_heap()) {}
+  Pair(const F& _first, const S& _second) : first(_first), second(_second) {}
 
-  std::shared_ptr<F> get_first() const { return this->first; }
-  std::shared_ptr<S> get_second() const { return this->second; }
+  F get_first() const { return this->first; }
+  S get_second() const { return this->second; }
 };
 
 }  // namespace collections
