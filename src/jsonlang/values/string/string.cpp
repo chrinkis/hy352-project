14f552d2d0da7b81a6150bbddced727d9714b1ae
Alex <csd4764@csd.uoc.gr>
2022-12-12 12:50:49 +0200

    feat(string): implement `String`

--- src/jsonlang/values/string/string.cpp
+++ src/jsonlang/values/string/string.cpp
@@ -1,1 +1,29 @@
 #include "string.h"
+
+using namespace jsonlang::values::string;
+
+String::String(const std::string& _data) : data(_data) {}
+
+String String::operator+(const String& other) const {
+  return String(this->data + other.data);
+}
+
+int String::get_size() const {
+  return 1;
+}
+
+bool String::is_emtpy() const {
+  return false;
+}
+
+bool String::has_key(const std::string& key) const {
+  return false;
+}
+
+const char* String::get_type() const {
+  return "string";
+}
+
+String::operator std::string() const {
+  return this->data;
+}
--- src/jsonlang/values/string/string.h
+++ src/jsonlang/values/string/string.h
@@ -1,10 +1,29 @@
 #pragma once
 
+#include "../value.h"
+
+#include <string>
+
 namespace jsonlang {
 namespace values {
 namespace string {
 
-class String {};
+class String : public Value {
+  std::string data;
+
+ public:
+  String(const std::string& _data);
+
+  String operator+(const String& other) const;
+
+ public:
+  int get_size() const;
+  bool is_emtpy() const;
+  bool has_key(const std::string& key) const;
+  const char* get_type() const;
+
+  operator std::string() const;
+};
 
 }  // namespace string
 }  // namespace values
