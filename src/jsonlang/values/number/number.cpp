8f064ba6cae1556fd9c3dc226671d843dfac1b7e
Nick <csd4619@csd.uoc.gr>
2022-12-12 15:04:12 +0200

    feat(number): implement 'Number'

--- src/jsonlang/values/number/number.cpp
+++ src/jsonlang/values/number/number.cpp
@@ -1,1 +1,62 @@
 #include "number.h"
+#include <string>
+
+using namespace jsonlang::values::number;
+
+Number::Number(const float _data) : data(_data) {}
+
+Number Number::operator+(const Number& other) const {
+  return Number(this->data + other.data);
+}
+
+Number Number::operator-(const Number& other) const {
+  return Number(this->data - other.data);
+}
+Number Number::operator*(const Number& other) const {
+  return Number(this->data * other.data);
+}
+
+Number Number::operator/(const Number& other) const {
+  return Number(this->data / other.data);
+}
+
+Number Number::operator%(const Number& other) const {
+  // float numbers do not allow modulo operator ("%").
+  return Number((int)this->data % (int)other.data);
+}
+
+bool Number::operator>(const Number& other) const {
+  return (this->data > other.data);
+}
+
+bool Number::operator>=(const Number& other) const {
+  return (this->data >= other.data);
+}
+
+bool Number::operator<(const Number& other) const {
+  return (this->data < other.data);
+}
+
+bool Number::operator<=(const Number& other) const {
+  return (this->data <= other.data);
+}
+
+int Number::get_size() const {
+  return 1;
+}
+
+bool Number::is_emtpy() const {
+  return false;
+}
+
+bool Number::has_key(const std::string& key) const {
+  return false;
+}
+
+const char* Number::get_type() const {
+  return "number";
+}
+
+Number::operator std::string() const {
+  return std::to_string(this->data);
+}
--- src/jsonlang/values/number/number.h
+++ src/jsonlang/values/number/number.h
@@ -1,10 +1,35 @@
 #pragma once
 
+#include "../value.h"
+
 namespace jsonlang {
 namespace values {
 namespace number {
 
-class Number {};
+class Number : public Value {
+  float data;
+
+ public:
+  Number(const float _data);
+
+  Number operator+(const Number& other) const;
+  Number operator-(const Number& other) const;
+  Number operator*(const Number& other) const;
+  Number operator/(const Number& other) const;
+  Number operator%(const Number& other) const;
+  bool operator>(const Number& other) const;
+  bool operator>=(const Number& other) const;
+  bool operator<(const Number& other) const;
+  bool operator<=(const Number& other) const;
+
+ public:
+  int get_size() const;
+  bool is_emtpy() const;
+  bool has_key(const std::string& key) const;
+  const char* get_type() const;
+
+  operator std::string() const;
+};
 
 }  // namespace number
 }  // namespace values
