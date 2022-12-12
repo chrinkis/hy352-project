b202b55fe2f0d1828cc84db441f256280050aa1d
Alex <csd4764@csd.uoc.gr>
2022-12-12 19:31:39 +0200

    feat(collections): implement `Pair`

--- src/collections/pair.h
+++ src/collections/pair.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <memory>
+
 namespace collections {
 
 template <typename F, typename S>
-class Pair {};
+class Pair {
+  std::shared_ptr<F> first;
+  std::shared_ptr<S> second;
+
+ public:
+  Pair(const F& _first, const S& _second)
+      : first(_first.clone_to_heap()), second(_second.clone_to_heap()) {}
+
+  std::shared_ptr<F> get_first() const { return this->first; }
+  std::shared_ptr<S> get_second() const { return this->second; }
+};
 
 }  // namespace collections
