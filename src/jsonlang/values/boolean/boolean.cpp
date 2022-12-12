f4e3c1d25abb5bea7c58974de8164af50f4b6e49
Nick <csd4619@csd.uoc.gr>
2022-12-12 16:45:40 +0200

    feat(boolean): implement `Boolean`

--- src/jsonlang/values/boolean/boolean.cpp
+++ src/jsonlang/values/boolean/boolean.cpp
@@ -1,1 +1,42 @@
 #include "boolean.h"
+
+using namespace jsonlang::values::boolean;
+
+Boolean::Boolean(const bool _data) : data(_data) {}
+
+Boolean Boolean::operator&&(const Boolean& other) const {
+  return Boolean(this->data && other.data);
+}
+
+Boolean Boolean::operator||(const Boolean& other) const {
+  return Boolean(this->data || other.data);
+}
+
+Boolean Boolean::operator!() const {
+  return Boolean(!this->data);
+}
+
+bool Boolean::operator==(const Boolean& other) const {
+  return (this->data == other.data);
+}
+
+bool Boolean::operator!=(const Boolean& other) const {
+  return (this->data != other.data);
+}
+
+int Boolean::get_size() const {
+  return 1;
+}
+bool Boolean::is_emtpy() const {
+  return false;
+}
+bool Boolean::has_key(const std::string& key) const {
+  return false;
+}
+const char* Boolean::get_type() const {
+  return "boolean";
+}
+
+Boolean::operator std::string() const {
+  return (this->data ? "true" : "false");
+}
--- src/jsonlang/values/boolean/boolean.h
+++ src/jsonlang/values/boolean/boolean.h
@@ -1,10 +1,31 @@
 #pragma once
 
+#include "../value.h"
+
 namespace jsonlang {
 namespace values {
 namespace boolean {
 
-class Boolean {};
+class Boolean : public Value {
+  bool data;
+
+ public:
+  Boolean(const bool _data);
+
+  Boolean operator&&(const Boolean& other) const;
+  Boolean operator||(const Boolean& other) const;
+  Boolean operator!() const;
+  bool operator==(const Boolean& other) const;
+  bool operator!=(const Boolean& other) const;
+
+ public:
+  int get_size() const;
+  bool is_emtpy() const;
+  bool has_key(const std::string& key) const;
+  const char* get_type() const;
+
+  operator std::string() const;
+};
 
 }  // namespace boolean
 }  // namespace values
