13ec65742604955ddba3ddfcca29731daffc39a9
Alex <csd4764@csd.uoc.gr>
2022-12-13 09:26:23 +0200

    feat(object): implement `Key`

--- src/jsonlang/values/object/key.cpp
+++ src/jsonlang/values/object/key.cpp
@@ -1,1 +1,15 @@
 #include "key.h"
+
+#include <string>
+
+using namespace jsonlang::values::object;
+
+Key::Key(const std::string& _key) : key(_key) {}
+
+std::string Key::get_key() const {
+  return this->key;
+}
+
+bool Key::operator<(const Key& other) const {
+  return this->key < other.key;
+}
--- src/jsonlang/values/object/key.h
+++ src/jsonlang/values/object/key.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <string>
+
 namespace jsonlang {
 namespace values {
 namespace object {
 
-class Key {};
+class Key {
+  const std::string key;
+
+ public:
+  Key(const std::string& key);
+
+  std::string get_key() const;
+
+  bool operator<(const Key& other) const;
+};
 
 }  // namespace object
 }  // namespace values
