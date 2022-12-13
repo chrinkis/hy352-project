e4708bf7b5289e70a492edd68771b66436e41037
Alex <csd4764@csd.uoc.gr>
2022-12-13 19:39:05 +0200

    feat(object): implement `Object`

--- src/jsonlang/values/object/object.cpp
+++ src/jsonlang/values/object/object.cpp
@@ -1,1 +1,79 @@
 #include "object.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+
+using namespace jsonlang::values;
+using namespace jsonlang::values::object;
+
+Object::Object(std::initializer_list<Pair> pairs) {
+  for (Object::Pair pair : pairs) {
+    Object::Key key = pair.get_first();
+    Object::ValuePtr value_ptr = pair.get_second();
+
+    this->data[key] = value_ptr;
+  }
+}
+
+Object::Object(const Object& other) {
+  for (auto pair : other.data) {
+    Object::Key key = pair.first;
+    Object::ValuePtr value_ptr = pair.second;
+
+    this->data[key] = Object::ValuePtr(value_ptr->clone_to_heap());
+  }
+}
+
+Value& Object::operator[](const std::string& key) {
+  auto resoult = this->data[Object::Key(key)];
+
+  assert(resoult);
+
+  return *resoult;
+}
+
+int Object::get_size() const {
+  return this->data.size();
+}
+
+bool Object::is_emtpy() const {
+  return this->data.empty();
+}
+
+bool Object::has_key(const std::string& key) const {
+  auto search = this->data.find(Object::Key(key));
+
+  return search != this->data.end();
+}
+
+const char* Object::get_type() const {
+  return "object";
+}
+
+Object::operator std::string() const {
+  std::string resoult = "{";
+
+  for (auto pair : this->data) {
+    resoult += pair.first.get_key();
+    resoult += ": ";
+    resoult += std::string(*pair.second);
+    resoult += ", ";
+  }
+
+  resoult += "}";
+
+  return resoult;
+}
+
+Value* Object::clone_to_heap() const {
+  return new Object(*this);
+}
+
+Object::Object(std::nullptr_t null) {
+  assert(0);
+}
+
+Object::Pair operator<<=(const Object::Key& key, const Value& value) {
+  return Object::Pair(key, Object::ValuePtr(value.clone_to_heap()));
+}
--- src/jsonlang/values/object/object.h
+++ src/jsonlang/values/object/object.h
@@ -1,11 +1,53 @@
 #pragma once
 
+#include "../value.h"
+
+#include <cstddef>
+#include <map>
+#include <memory>
+
+#include "key.h"
+#include "pair.h"
+
 namespace jsonlang {
 namespace values {
 namespace object {
 
-class Object {};
+class Object : public Value {
+ public:
+  using Key = jsonlang::values::object::Key;
+  using ValuePtr = std::shared_ptr<Value>;
+  using Pair = collections::Pair<Key, ValuePtr>;
+
+ private:
+  std::map<Key, ValuePtr> data;
+
+ private:
+  Object(const Object& other);
+
+ public:
+  Object(std::initializer_list<Pair>);
+
+  Value& operator[](const std::string& key);
+
+ public:
+  int get_size() const;
+  bool is_emtpy() const;
+  bool has_key(const std::string& key) const;
+  const char* get_type() const;
+
+  operator std::string() const;
+
+  Value* clone_to_heap() const;
+
+ public:
+  Object(std::nullptr_t null);
+};
 
 }  // namespace object
 }  // namespace values
 }  // namespace jsonlang
+
+jsonlang::values::object::Object::Pair operator<<=(
+    const jsonlang::values::object::Object::Key& key,
+    const jsonlang::values::Value& value);
