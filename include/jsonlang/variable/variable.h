dffbbad164368e6e1c3a16a8d10ccfced6b2879f
Alex <csd4764@csd.uoc.gr>
2022-12-28 13:52:06 +0200

    feat(variable): add `Variable` defintion

--- include/jsonlang/variable/variable.h
+++ include/jsonlang/variable/variable.h
@@ -1,9 +1,67 @@
 #pragma once
 
+#include <collections/sequence.h>
+#include <jsonlang/values/value.h>
+#include <memory>
+#include <string>
+
 namespace jsonlang {
 namespace variable {
 
-class Variable {};
+class Variable {
+ public:
+  using ValuePtr = std::shared_ptr<values::Value>;
+  using ValuePtrSequence = collections::Sequence<ValuePtr>;
+
+ private:
+  ValuePtr value_ptr;
+
+ private:
+  values::Value* parent = nullptr;
+  int index_int = -1;
+  std::string index_str;
+
+ private:
+  Variable(const values::Value& value,
+           values::Value* _parent,
+           const std::string& _index);
+
+  Variable(const values::Value& value, values::Value* _parent, int _index);
+
+ public:
+  Variable(const values::Value& value);
+  Variable(const Variable& other);
+
+ public:
+  values::Value& get_parent() const;
+
+ public:
+  Variable& operator=(const values::Value& value);
+  Variable& operator+=(const values::Value& value);
+  Variable& operator+=(const ValuePtrSequence& value);
+  Variable operator[](const std::string& _index);
+  Variable operator[](int _index);
+  void erase();
+};
+
+Variable operator+(const Variable& left, const Variable& right);
+Variable operator-(const Variable& left, const Variable& right);
+Variable operator*(const Variable& left, const Variable& right);
+Variable operator/(const Variable& left, const Variable& right);
+Variable operator%(const Variable& left, const Variable& right);
+
+// `bool` or `values::Boolean` ?
+bool operator>(const Variable& left, const Variable& right);
+bool operator>=(const Variable& left, const Variable& right);
+bool operator<(const Variable& left, const Variable& right);
+bool operator<=(const Variable& left, const Variable& right);
+
+bool operator&&(const Variable& left, const Variable& right);
+bool operator||(const Variable& left, const Variable& right);
+bool operator!(const Variable& left);
+
+bool operator==(const Variable& left, const Variable& right);
+bool operator!=(const Variable& left, const Variable& right);
 
 }  // namespace variable
 }  // namespace jsonlang
