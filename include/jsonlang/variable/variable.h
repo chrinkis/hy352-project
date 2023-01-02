1e37c88084ad668dd62c01038d18ed3efb8c4caf
Alex <csd4764@csd.uoc.gr>
2023-01-02 09:44:19 +0200

    refactor(variable): replace operator `+=` with `,`

--- include/jsonlang/variable/variable.h
+++ include/jsonlang/variable/variable.h
@@ -37,8 +37,7 @@ class Variable {
 
  public:
   Variable& operator=(const values::Value& value);
-  Variable& operator+=(const values::Value& value);
-  Variable& operator+=(const ValuePtrSequence& value);
+  Variable& operator,(const values::Value& value);
   const Variable operator[](const std::string& _index) const;
   const Variable operator[](int _index) const;
   void erase();
