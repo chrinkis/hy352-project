4fd3b06373e91205c17d8c97e87270dd7b53df06
Nick <csd4619@csd.uoc.gr>
2023-01-04 03:39:50 +0200

    feat(variable): implement variable operator overloading

--- src/variable/variable.cpp
+++ src/variable/variable.cpp
@@ -19,3 +19,58 @@ Appender Variable::operator+=(const values::Value& value) {
 
   return (appender, value);
 }
+
+Variable operator+(const Variable& left, const Variable& right) {
+  return Variable(*left.get_value().add_op(right.get_value()));
+}
+
+Variable operator-(const Variable& left, const Variable& right) {
+  return Variable(*left.get_value().sub_op(right.get_value()));
+}
+
+Variable operator*(const Variable& left, const Variable& right) {
+  return Variable(*left.get_value().mul_op(right.get_value()));
+}
+
+Variable operator/(const Variable& left, const Variable& right) {
+  return Variable(*left.get_value().div_op(right.get_value()));
+}
+
+Variable operator%(const Variable& left, const Variable& right) {
+  return Variable(*left.get_value().mod_op(right.get_value()));
+}
+
+bool operator>(const Variable& left, const Variable& right) {
+  return left.get_value().gt_op(right.get_value());
+}
+
+bool operator>=(const Variable& left, const Variable& right) {
+  return left.get_value().ge_op(right.get_value());
+}
+
+bool operator<(const Variable& left, const Variable& right) {
+  return left.get_value().lt_op(right.get_value());
+}
+bool operator<=(const Variable& left, const Variable& right) {
+  return left.get_value().le_op(right.get_value());
+}
+
+bool operator&&(const Variable& left, const Variable& right) {
+  return left.get_value().and_op(right.get_value());
+}
+
+bool operator||(const Variable& left, const Variable& right) {
+  return left.get_value().or_op(right.get_value());
+}
+
+bool operator!(const Variable& left) {
+  return left.get_value().not_op();
+}
+
+bool operator==(const Variable& left, const Variable& right) {
+  return left.get_value().eq_op(right.get_value());
+}
+
+bool operator!=(const Variable& left, const Variable& right) {
+  return left.get_value().neq_op(right.get_value());
+}
