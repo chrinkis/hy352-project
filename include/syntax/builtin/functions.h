246ee173151988e22b548490d799d3937495f6fb
nick <csd4619@csd.uoc.gr>
2023-01-10 12:28:45 +0200

    feat(functions): implement `size_of`

--- include/syntax/builtin/functions.h
+++ include/syntax/builtin/functions.h
@@ -1,9 +1,23 @@
 #pragma once
 
-#define SIZE_OF  // TODO
+#include <jsonlang/values/value.h>
+#include <jsonlang/variable/variable.h>
+
+using namespace jsonlang::values;
+using namespace jsonlang::variable;
+
+#define SIZE_OF(val_or_var) size_of(val_or_var)
 
 #define IS_EMPTY  // TODO
 
 #define HAS_KEY  // TODO
 
 #define TYPE_OF  // TODO
+
+int size_of(const Value& value) {
+  return value.get_size();
+}
+
+int size_of(const Variable& variable) {
+  return variable.get_value().get_size();
+}
