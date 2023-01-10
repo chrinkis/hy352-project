3323673e2da7ce5e399cf8a7821d0ec33676b2ba
Alex <csd4764@csd.uoc.gr>
2023-01-10 20:29:59 +0200

    feat(jsonlang): add operator overloading

---
+++ include/syntax/operators.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <jsonlang/values/value.h>
+#include <jsonlang/variable/variable.h>
+#include "jsonlang/values/boolean.h"
+
+using namespace jsonlang::values;
+using namespace jsonlang::variable;
+
+Variable operator+(const Value& left, const Value& right) {
+  return left.add_op(right);
+}
+
+Variable operator-(const Value& left, const Value& right) {
+  return left.sub_op(right);
+}
+
+Variable operator*(const Value& left, const Value& right) {
+  return left.mul_op(right);
+}
+Variable operator/(const Value& left, const Value& right) {
+  return left.div_op(right);
+}
+
+Variable operator%(const Value& left, const Value& right) {
+  return left.mod_op(right);
+}
+
+Boolean operator==(const Value& left, const Value& right) {
+  return left.eq_op(right);
+}
+
+Boolean operator!=(const Value& left, const Value& right) {
+  return left.neq_op(right);
+}
+
+Boolean operator>(const Value& left, const Value& right) {
+  return left.gt_op(right);
+}
+
+Boolean operator>=(const Value& left, const Value& right) {
+  return left.ge_op(right);
+}
+
+Boolean operator<(const Value& left, const Value& right) {
+  return left.lt_op(right);
+}
+
+Boolean operator<=(const Value& left, const Value& right) {
+  return left.le_op(right);
+}
+
+Boolean operator!(const Value& left) {
+  return left.not_op();
+}
+
+Boolean operator&&(const Value& left, const Value& right) {
+  return left.and_op(right);
+}
+
+Boolean operator||(const Value& left, const Value& right) {
+  return left.or_op(right);
+}
