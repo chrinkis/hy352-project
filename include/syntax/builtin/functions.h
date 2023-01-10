875aa865634d4b5fd21605494b6d3bad1dbc45bb
nick <csd4619@csd.uoc.gr>
2023-01-10 12:35:05 +0200

    feat(functions): implement `is_empty`

--- include/syntax/builtin/functions.h
+++ include/syntax/builtin/functions.h
@@ -1,9 +1,23 @@
 #pragma once
 
+#include <jsonlang/values/value.h>
+#include <jsonlang/variable/variable.h>
+
+using namespace jsonlang::values;
+using namespace jsonlang::variable;
+
 #define SIZE_OF  // TODO
 
-#define IS_EMPTY  // TODO
+#define IS_EMPTY(val_or_var) is_empty(val_or_var)
 
 #define HAS_KEY  // TODO
 
 #define TYPE_OF  // TODO
+
+bool is_empty(const Value& value) {
+  return value.is_emtpy();
+}
+
+bool is_empty(const Variable& variable) {
+  return variable.get_value().is_emtpy();
+}
