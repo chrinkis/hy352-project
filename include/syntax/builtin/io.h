72f6917291e41820f5b016317e4c9dd8bf0cc895
Alex <csd4764@csd.uoc.gr>
2023-01-10 13:58:39 +0200

    fix(jsonlang): print sequence in the same line

--- include/syntax/builtin/io.h
+++ include/syntax/builtin/io.h
@@ -10,14 +10,16 @@
 
 struct Printer {
   const Printer& operator,(const jsonlang::values::Value& value) const {
-    std::cout << value << std::endl;
+    std::cout << value;
 
     return *this;
   }
 
   const Printer& operator,(const jsonlang::variable::Variable& variable) const {
-    std::cout << variable << std::endl;
+    std::cout << variable;
 
     return *this;
   }
+
+  ~Printer() { std::cout << std::endl; }
 };
