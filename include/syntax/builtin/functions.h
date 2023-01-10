f7e2db277f5bdc0df851a1ddbb2bd84db668071e
nick <csd4619@csd.uoc.gr>
2023-01-10 12:48:49 +0200

    feat(functions): implement `type_of`

--- include/syntax/builtin/functions.h
+++ include/syntax/builtin/functions.h
@@ -1,9 +1,23 @@
 #pragma once
 
+#include <jsonlang/values/value.h>
+#include <jsonlang/variable/variable.h>
+
+using namespace jsonlang::values;
+using namespace jsonlang::variable;
+
 #define SIZE_OF  // TODO
 
 #define IS_EMPTY  // TODO
 
 #define HAS_KEY  // TODO
 
-#define TYPE_OF  // TODO
+#define TYPE_OF(val_or_var) type_of(val_or_var)
+
+std::string type_of(const Value& value) {
+  return value.get_type();
+}
+
+std::string type_of(const Variable& variable) {
+  return variable.get_value().get_type();
+}
